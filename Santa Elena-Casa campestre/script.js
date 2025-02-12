e4af43dd6b5c205cb6440f989f2cd1fef3ce6e83
Jeronimo Narvaez <jeronimo322607220@gmail.com>
2025-02-12 16:54:00 -0500

    Se agregaron cambios significativos en el css y el HTML

--- Santa Elena-Casa campestre/script.js
+++ Santa Elena-Casa campestre/script.js
@@ -1,1 +1,13 @@
 alert("This is my Js")
+window.addEventListener("scroll", function () {
+    let header = document.querySelector(".header");
+    let logo = document.querySelector(".logo");
+    let links = document.querySelectorAll("nav ul li a");
+
+    if (window.scrollY > 50) {
+        header.classList.add("shrink");
+    } else {
+        header.classList.remove("shrink");
+    }
+});
+
