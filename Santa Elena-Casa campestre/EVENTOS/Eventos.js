e08e2f349239ec3b96df68b55b9f719e70e288d2
SantiagoTr02 <trianacarvajalsantiago@gmail.com>
2025-02-26 14:09:27 -0500

    Se agrego el js
    Se agrego el js de EVENTOS

---
+++ Santa Elena-Casa campestre/EVENTOS/Eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    Swal.fire({
+        title: "Bienvenido",
+        text: "Bienvenido a nuestros EVENTOS",
+        icon: "success",
+        confirmButtonText: "Aceptar"
+    });
+});
---
+++ Santa Elena-Casa campestre/EVENTOS/ReservarEventos.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    Swal.fire({
+        title: "Bienvenido",
+        text: "Bienvenido a la RESERVA DE EVENTOS",
+        icon: "success",
+        confirmButtonText: "Aceptar"
+    });
+});
